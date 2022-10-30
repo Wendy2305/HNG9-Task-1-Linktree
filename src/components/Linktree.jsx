0ca52b4d0b694730fd239b3153d87ac17872e719
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2022-10-30 15:51:45 +0100

    second-commit

--- src/components/Linktree.jsx
+++ src/components/Linktree.jsx
@@ -4,19 +4,25 @@ import github from '../images/github_icon.png'
 import zuri from '../images/zuri.png'
 import i4g from '../images/I4G.png'
 import slack from '../images/slack.png'
+import '../App.css'
 const Linktree = () => {
   return (
     <div className="entire-page">
-        <img src={profile_pic} alt="Wendy Nweje"/>
-        <h1>Chinwendu Nweje</h1>
-        <a id= "twitter" href="https://twitter.com/chi_thetechsis"> Twitter Link</a>
-        <a id="btn__zuri" href="https://training.zuri.team/"> Zuri Team</a>
-        <a id="books" href="http://books.zuri.team/"> Zuri Books</a>
-        <a id = "book__python" href="https://books.zuri.team/"> Python Books</a>
-        <a id ="pitch" href="https://background.zuri.team/"> Background Checks for Coders</a>
-        <a id="book__design" href="https://books.zuri.team/design-rules"> Design Books</a>
 
-        <div>
+        <img src={profile_pic} alt="Wendy Nweje"/>
+
+        <h1>Chinwendu Nweje</h1>
+
+        <div className='links'>
+          <a className="button-link" id= "twitter" href="https://twitter.com/chi_thetechsis"> Twitter Link</a>
+          <a className="button-link" id="btn__zuri" href="https://training.zuri.team/"> Zuri Team</a>
+          <a className="button-link" id="books" href="http://books.zuri.team/"> Zuri Books</a>
+          <a className="button-link" id = "book__python" href="https://books.zuri.team/"> Python Books</a>
+          <a className="button-link"id ="pitch" href="https://background.zuri.team/"> Background Checks for Coders</a>
+          <a className="button-link" id="book__design" href="https://books.zuri.team/design-rules"> Design Books</a>
+        </div>
+
+        <div className="socials">
             <img src={github} alt="GitHub Logo"/>
             <img src={slack} alt="Slack Logo"/>
         </div>
